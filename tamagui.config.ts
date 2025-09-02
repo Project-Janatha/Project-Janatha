b814731de78d3c454cddba8b1be32f134c5bf179
theabhiramr <ramachandran.abhiram@gmail.com>
2025-09-02 17:05:13 -0400

    updated tabs and created a config

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,12 +1,46 @@
 import { defaultConfig } from '@tamagui/config/v4'
-import { createTamagui } from 'tamagui'
+import { createFont, createTamagui, isWeb } from 'tamagui'
 
-export const config = createTamagui(defaultConfig)
+const systemFont = createFont({
+  family: isWeb ? 'Anek Latin, Arial, sans-serif' : 'System',
+  size: {
+    1: 12,
+    2: 14,
+    3: 15,
+  },
+  lineHeight: {
+    // 1 will be 22
+    2: 22,
+  },
+  weight: {
+    1: '300',
+    // 2 will be 300
+    3: '600',
+  },
+  letterSpacing: {
+    1: 0,
+    2: -1,
+    // 3 will be -1
+  },
+  // (native only) swaps out fonts by face/style
+  face: {
+    300: { normal: 'InterLight', italic: 'InterItalic' },
+    600: { normal: 'InterBold' },
+  },
+})
 
-export default config
+const config = createTamagui({
+  ...defaultConfig,
+  media: {
+    ...defaultConfig.media,
+    // add your own media queries here, if wanted
+  },
+})
 
-export type Conf = typeof config
+type OurConfig = typeof config
 
 declare module 'tamagui' {
-  interface TamaguiCustomConfig extends Conf {}
+  interface TamaguiCustomConfig extends OurConfig {}
 }
+
+export default config
