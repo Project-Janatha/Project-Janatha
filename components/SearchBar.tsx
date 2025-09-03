13b0d0367e241df12289ccf7313710b4789f02a1
theabhiramr <ramachandran.abhiram@gmail.com>
2025-09-03 13:47:56 -0400

    created index file for more easy exports

---
+++ components/SearchBar.tsx
@@ -0,0 +1,23 @@
+/**
+ * SearchBar.tsx
+ * 
+ * Om Sri Cinmaya Sadgurave Namaha. Om Sri Gurubyo Namaha.
+ * Author: Abhiram Ramachandran
+ * Date Authored: September 2, 2025
+ * Last Date Modified: September 2, 2025
+ * 
+ * This file exports a SearchBar component that provides a search input field with an icon.
+ * 
+ * Dependencies:
+ * - @rnmapbox/maps: For rendering maps and handling map-related functionalities.
+ */
+import { Input, Button } from 'tamagui';
+import { Search } from '@tamagui/lucide-icons';
+
+export default function SearchBar(props: any) {
+  return (
+  <>
+    <Input placeholder="Type here" />
+  </>
+  )
+}
