3ff677a16d41ba2b91f2c93654fe2702ead4b759
theabhiramr <ramachandran.abhiram@gmail.com>
2025-09-07 22:33:33 -0400

    .

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,12 +1,28 @@
-import { Anchor, Paragraph, View, XStack } from 'tamagui'
+import React, { useContext } from 'react'
+import { Anchor, H2, Paragraph, View, Button, XStack, YStack, Input } from 'tamagui'
+import { UserContext } from 'components'
 
-export default function ModalScreen() {
+export default function LogInScreen(props) {
+  const { login, signup } = useContext(UserContext);
   return (
-    <View flex={1} items="center" justify="center">
-      <XStack gap="$2">
-        <Paragraph text="center">Made by</Paragraph>
+    <View flex={1} bg="$background" p="$4">
+      <YStack gap="$4">
+        <H2 style='bold'>Log In</H2>
+        <Input placeholder="Username" />
+        <Input placeholder="Password" secureTextEntry />
+
+        <XStack gap="$4">
+          <Button bg="orange" onPress={() => login('username', 'password')}>
+            Login
+          </Button>
+          <Button onPress={() => signup('username', 'password')}>
+            Sign Up
+          </Button>
+        </XStack>
+
+        {/* <Paragraph text="center">Made by</Paragraph>
         <Anchor color="$blue10" href="https://twitter.com/natebirdman" target="_blank">
-          @natebirdman,
+          @natebirdmran,
         </Anchor>
         <Anchor
           color="$accent10"
@@ -15,8 +31,8 @@ export default function ModalScreen() {
           rel="noreferrer"
         >
           give it a ⭐️
-        </Anchor>
-      </XStack>
+        </Anchor> */}
+      </YStack>
     </View>
   )
 }
