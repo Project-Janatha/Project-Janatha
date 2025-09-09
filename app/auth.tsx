89f8efacdaa222181b05d5d2b8d8e0d42d70fbf0
theabhiramr <ramachandran.abhiram@gmail.com>
2025-09-08 22:34:01 -0400

    updated auth page

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,39 +1,50 @@
-import React, { useContext } from 'react'
-import { Anchor, H2, Paragraph, View, Button, XStack, YStack, Input } from 'tamagui'
+import React, { useState, useContext } from 'react'
+import { Anchor, H2, Paragraph, View, Button, XStack, Form, Input, YStack } from 'tamagui'
 import { UserContext } from 'components'
 
 export default function AuthScreen(props) {
   const { login, signup, error, loading } = useContext(UserContext);
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleUsername = ({ target }) => setUsername(target.value);
+  const handlePassword = ({ target }) => setPassword(target.value);
   return (
-    <View flex={1} bg="$background" p="$4">
-      <YStack gap="$4">
+    <YStack 
+    flex={1} 
+    bg="$background" 
+    p="$4"
+    justify="center"
+    items={"center"}>
+      <Form
+        items="center"
+        width={'75%'}
+        height={'auto'}
+        maxW={600}
+        gap="$2"
+        onSubmit={() => login(username, password)}
+        borderWidth={1}
+        rounded="$4"
+        bg="$background"
+        borderColor="$borderColor"
+        p="$8"
+      >
         <H2 fontWeight={'$5'}>Log In</H2>
         {error && <Paragraph color="red">{error}</Paragraph>}
         <Input placeholder="Username" />
         <Input placeholder="Password" secureTextEntry />
 
         <XStack gap="$4">
-          <Button bg="orange" onPress={() => login('username', 'password')}>
-            Login
-          </Button>
-          <Button onPress={() => signup('username', 'password')}>
+          <Form.Trigger asChild>
+            <Button bg="orange">
+              Login
+            </Button>
+          </Form.Trigger>
+          <Button onPress={() => signup(username, password)}>
             Sign Up
           </Button>
         </XStack>
-
-        {/* <Paragraph text="center">Made by</Paragraph>
-        <Anchor color="$blue10" href="https://twitter.com/natebirdman" target="_blank">
-          @natebirdmran,
-        </Anchor>
-        <Anchor
-          color="$accent10"
-          href="https://github.com/tamagui/tamagui"
-          target="_blank"
-          rel="noreferrer"
-        >
-          give it a ⭐️
-        </Anchor> */}
-      </YStack>
-    </View>
+      </Form>
+    </YStack>
   )
 }
