a3d4669771447cbd80dc107611e303c12369c264
theabhiramr <ramachandran.abhiram@gmail.com>
2025-10-11 21:59:11 -0400

    finally got it to build

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'nativewind/babel'],
     plugins: [
-      ['nativewind/babel', { mode: 'native' }],
       'react-native-worklets/plugin',
       'react-native-worklets-core/plugin',
     ],
